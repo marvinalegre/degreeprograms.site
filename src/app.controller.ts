ab58d47a336aa08aa839ed07f6fec286c21c5024
Marvin Alegre <alegremrvn@protonmail.com>
2023-08-04 09:27:07 +0800

    feat: serve home page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+
 import { AppService } from './app.service';
+
+import { Response } from 'express'
+
+import { join } from 'path'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getThemAll(){
-    return this.appService.findAll();
+  // getThemAll(){
+  //   return this.appService.findAll();
+  // }
+  serveHome(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'client', 'home.html'))
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { join } from 'path'
+
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
