72c0c82bc35039119088ff9a2eba0d46b05863a5
Marvin Alegre <alegremrvn@protonmail.com>
2023-08-04 09:15:33 +0800

    chore: add mongodb

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { MongoClient } from 'mongodb'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  findAll() {
+    const client = new MongoClient(process.env.MONGO_URI)
+
+    async function run() {
+      try {
+  
+        const database = client.db("nestjs")
+        const cats = database.collection("cats")
+  
+        const query = {}
+        const options = {}
+  
+        const cursor = cats.find(query, options)
+  
+        if ((await cats.countDocuments(query)) === 0) {
+          console.log('No documents found!')
+        }
+  
+        for await (const doc of cursor) {
+          console.log(doc)
+        }
+      } finally {
+        await client.close()
+      }
+    }
+
+    run().catch(console.log)
   }
 }
